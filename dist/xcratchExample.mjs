3414e85a4e8f4ebac02af8447962e72d92f0fe23
EngineeringEducationSteve <161227344+EngineeringEducationSteve@users.noreply.github.com>
2025-01-01 10:06:35 -0800

    Removed second block

--- dist/xcratchExample.mjs
+++ dist/xcratchExample.mjs
@@ -1360,23 +1360,6 @@ var ExtensionBlocks = /*#__PURE__*/function () {
             }
           }
         }],
-	      [{
-          opcode: 'Capitalize2',
-          blockType: BlockType$1.REPORTER,
-          blockAllThreads: false,
-          text: formatMessage({
-            id: 'xcratchExample.cap2',
-            default: 'Capitalize [SCRIPT]',
-            description: 'execute javascript for example'
-          }),
-          func: 'doIt',
-          arguments: {
-            SCRIPT: {
-              type: ArgumentType$1.STRING,
-              defaultValue: 'Hello, Again'
-            }
-          }
-        }],
         menus: {}
       };
     }
