aa4b1555966ce80eda0e1dfa61f95403c180b7df
EngineeringEducationSteve <161227344+EngineeringEducationSteve@users.noreply.github.com>
2025-01-01 09:53:02 -0800

    Update xcratchExample.mjs

--- dist/xcratchExample.mjs
+++ dist/xcratchExample.mjs
@@ -1264,7 +1264,7 @@ var log$1 = /*@__PURE__*/getDefaultExportFromCjs(log);
 
 var en = {
 	"xcratchExample.name": "Xcratch Example",
-	"xcratchExample.doIt": "Capitalize []",
+	"xcratchExample.doIt": "Capitalize [SCRIPT]",
 	
 };
 var ja = {
